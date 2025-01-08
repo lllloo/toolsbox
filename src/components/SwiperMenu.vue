d759a0e8d129a655793ad96b59d20dbb8431f5fd
Barney <muki.barney@gmail.com>
2025-01-08 12:07:35 +0800

    feat: 更新 SwiperMenu 組件，新增滑動功能及中心對齊

--- src/components/SwiperMenu.vue
+++ src/components/SwiperMenu.vue
@@ -3,20 +3,30 @@ import { register } from 'swiper/element/bundle'
 register()
 
 const checked = ref(1)
+const swiperEl = ref()
+
+const check = (index: number) => {
+  checked.value = index
+  swiperEl.value.swiper.slideTo(index - 1)
+}
 </script>
 <template>
-  <swiper-container
-    class="w-full"
-    :slides-per-view="'auto'"
-    :space-between="10"
-    :pagination="false"
-  >
-    <swiper-slide v-for="i in 10" :key="i" class="w-auto">
-      <Button :variant="checked === i ? 'default' : 'secondary'" @click="checked = i">
-        Slide {{ i }}
-      </Button>
-    </swiper-slide>
-  </swiper-container>
+  <div class="w-full">
+    <swiper-container
+      class="size-full"
+      slides-per-view="auto"
+      :space-between="10"
+      :pagination="false"
+      :centered-slides="true"
+      ref="swiperEl"
+    >
+      <swiper-slide v-for="i in 10" :key="i" class="flex-1">
+        <Button :variant="checked === i ? 'default' : 'secondary'" @click="check(i)">
+          Slide {{ i }}
+        </Button>
+      </swiper-slide>
+    </swiper-container>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
